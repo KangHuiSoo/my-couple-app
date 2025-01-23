3a580b44ba14896afa6bd026de0183f8de706738
KangHuiSoo <a23390@naver.com>
2025-01-23 14:12:12 +0900

    google-map 적용 및 장소추가 페이지 퍼블리싱

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+//     GMSServices.provideAPIKey("AIzaSyDwxN1ovzf72AcXl6A6WCwWytCLcu5f6Rs")
+    GMSServices.provideAPIKey("AIzaSyDwxN1ovzf72AcXl6A6WCwWytCLcu5f6Rs")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ lib/screen/place_add_screen.dart
@@ -0,0 +1,148 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:my_couple_app/component/custom_button.dart';
+import 'package:my_couple_app/component/custom_text_field.dart';
+import 'package:my_couple_app/const/colors.dart';
+
+class PlaceAddScreen extends StatefulWidget {
+  const PlaceAddScreen({super.key});
+
+  @override
+  State<PlaceAddScreen> createState() => _PlaceAddScreenState();
+}
+
+class _PlaceAddScreenState extends State<PlaceAddScreen> {
+  final List<String> imageUrls = [
+    'assets/images/default_background.jpg',
+    'assets/images/default_background.jpg',
+    'assets/images/default_background.jpg',
+    'assets/images/default_background.jpg',
+  ];
+
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(35.19343151233912, 129.0504196440337);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("장소추가"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 장소명 입력 필드
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: CustomTextField(hintText: "장소명"),
+            ),
+            SizedBox(height: 8.0),
+            // Google Map 위젯
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition:
+                  CameraPosition(target: _center, zoom: 15.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 8.0),
+
+            // 정보 텍스트 영역
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text('헌스시',
+                          style: TextStyle(
+                              fontSize: 18.0, fontWeight: FontWeight.bold)),
+                      SizedBox(width: 10.0),
+                      Text('식당', style: TextStyle(color: Colors.grey)),
+                    ],
+                  ),
+                  SizedBox(height: 4.0),
+                  Text('부산광역시 해운대구 중동2로 2길',
+                      style: TextStyle(fontSize: 14.0)),
+                  Row(
+                    children: List.generate(
+                      5,
+                          (index) => Icon(Icons.star, color: Colors.orange, size: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 8.0),
+            // 이미지 가로 스크롤 영역
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: SizedBox(
+                height: 100, // 고정된 높이를 설정
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: imageUrls.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.asset(
+                          imageUrls[index],
+                          width: 88, // 이미지 가로 크기
+                          height: 88, // 이미지 세로 크기
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+
+            SizedBox(height: 16.0),
+
+            // 버튼 영역
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, // 버튼 간격 조정
+                children: [
+                  Expanded(
+                    child: CustomButton(
+                      onPressed: () {},
+                      backgroundColor: Colors.white,
+                      textColor: PRIMARY_COLOR,
+                      buttonText: "취소",
+                    ),
+                  ),
+                  SizedBox(width: 16.0), // 버튼 간격 추가
+                  Expanded(
+                    child: CustomButton(
+                      onPressed: () {},
+                      textColor: Colors.white,
+                      backgroundColor: PRIMARY_COLOR,
+                      buttonText: "추가",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
