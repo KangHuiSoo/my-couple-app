3c30fe734d2b57cc2fac0c2b8a1815a8585c7490
KangHuiSoo <a23390@naver.com>
2025-01-29 14:09:47 +0900

    공통 컴포넌트 파일 생성

---
+++ lib/core/ui/component/profile_photo.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/colors.dart';
+
+class ProfilePhoto extends StatelessWidget {
+  final double outsideSize;
+  final double insideSize;
+  final double radius;
+  final String imageUrl;
+  const ProfilePhoto({super.key, required this.outsideSize, required this.insideSize, required this.imageUrl, required this.radius});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        // 외곽 테두리
+        SizedBox(
+          width: outsideSize,
+          height: outsideSize,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: PRIMARY_COLOR,
+                width: 2,
+              ),
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+        // 내부 테두리
+        SizedBox(
+          width: insideSize,
+          // CircleAvatar 크기 + 내부 테두리 두께
+          height: insideSize,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white, // 내부 테두리 색상
+            ),
+          ),
+        ),
+        // 프로필 이미지
+        CircleAvatar(
+          radius: radius, // 프로필 이미지 크기
+          backgroundColor: PRIMARY_COLOR,
+          backgroundImage: AssetImage(
+              imageUrl), // 이미지 경로
+        ),
+      ],
+    );
+  }
+}
