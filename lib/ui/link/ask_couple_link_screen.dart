0cf8b86cf51d15e6e342de03653c8c06be04a3d9
KangHuiSoo <a23390@naver.com>
2025-02-04 12:02:04 +0900

    커플링크 프로세스 변경

---
+++ lib/ui/link/ask_couple_link_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:my_couple_app/core/constants/colors.dart';
+import 'package:my_couple_app/core/ui/component/custom_button.dart';
+import 'package:my_couple_app/core/ui/component/custom_text_field.dart';
+import 'package:my_couple_app/core/ui/component/custom_title.dart';
+import 'package:my_couple_app/ui/home/home_screen.dart';
+
+import 'couple_link_screen.dart';
+
+
+class AskCoupleLinkScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // 상단 로고
+                Column(
+                  children: [
+                    // SvgPicture.asset(
+                    //   'assets/images/welcome_logo.svg',
+                    //   width: 144,
+                    //   height: 34,
+                    // ),
+                    Text('연인과 연결하기', style: TextStyle(
+                      fontFamily: 'okddung',
+                      color: PRIMARY_COLOR,
+                      fontSize: 30.0
+                    )),
+                    SizedBox(height: 15.0),
+                    Text("연인에게 커플 연결을 요청하거나"),
+                    Text("요청받은 응답 링크를 직접 입력하여 연인과 연결하세요")
+                  ],
+                ),
+
+                SizedBox(height: 40),
+
+                // 로그인 버튼
+                CustomButton(
+                  onPressed: () {},
+                  buttonText: "커플 요청하기",
+                  textColor: Colors.white,
+                  backgroundColor: PRIMARY_COLOR,
+                ),
+                SizedBox(height: 16.0),
+                CustomButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => CoupleLinkScreen()));
+                  },
+                  buttonText: "링크 입력하기",
+                  textColor: PRIMARY_COLOR,
+                  backgroundColor: Colors.white,
+                ),
+                SizedBox(height: 8.0),
+                TextButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> HomeScreen()));
+                }, child: Text('나중에하기', style: TextStyle(color: Colors.grey)))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
