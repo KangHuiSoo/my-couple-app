6fedc371819c8150a528374ed4f43c4adaa5c6e2
KangHuiSoo <a23390@naver.com>
2025-02-07 14:04:52 +0900

    ProviderScope 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:my_couple_app/ui/login/login_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized(); //Flutter의 비동기 작업 초기화
   await initializeDateFormatting(); //로케일 데이터 초기화
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
